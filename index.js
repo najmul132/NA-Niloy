291ccbe81889650de6ed0d2b06f92fc55403a2ee
najmul132 <69905413+najmul132@users.noreply.github.com>
2020-08-20 15:58:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Sep, 28, 2020',
+  birthdate: 'oct, 28, 2020',
   name: 'Aporna'
 };
 
